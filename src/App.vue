73f1bf1bcee61b9ce4b4a03ad0a0c04f3067f585
Hadi <hadihaz1999@gmail.com>
2023-10-15 10:52:28 +0330

    create and config project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>home</div>
+
+  <RouterView />
+</template>
+
+<style >
+
+</style>
